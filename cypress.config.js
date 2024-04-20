ca8d055996d7b17b07ec0d3b08156e1272eba1bd
pavani-jandhyala <pavni.jandhyala@gmail.com>
2024-04-20 15:20:41 -0400

    added .js and json files

---
+++ cypress.config.js
@@ -0,0 +1,15 @@
+const { defineConfig } = require('cypress');
+const {downloadFile} = require('cypress-downloadfile/lib/addPlugin')
+
+
+module.exports = defineConfig({
+  e2e: {
+    watchForFileChanges:false,
+    video:true,
+    defaultCommandTimeout:4000,
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+      on('task',{downloadFile})
+    },
+  },
+});
